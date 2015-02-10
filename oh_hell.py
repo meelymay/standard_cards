85c3dce9e7d060b65e19ef1cb7d251c4d252b70d
Amelia Arbisser <aarbisser@trifacta.com>
2015-02-10 13:34:25 -0800

    oh hell works, discovered deck bug two 3 of spades

--- oh_hell.py
+++ oh_hell.py
@@ -13,6 +13,7 @@ class Player(object):
         self.hand += cards
 
     def display_hand(self):
+        print str(self)+"'s hand:"
         print '----------'
         for card in self.hand:
             print card
@@ -22,7 +23,7 @@ class Player(object):
         legal = [b for b in range(len(self.hand)+1) if b != forbidden]
         self.bid = random.choice(legal)
 
-        print self,'makes bid',self.bid
+        print self,'bids',self.bid
         return self.bid
 
     def play(self, suit):
@@ -32,16 +33,21 @@ class Player(object):
         card = random.choice(legal)
         self.hand.remove(card)
 
-        print self,'plays',card
+        print self,'plays\t\t\t',card
         return card
 
     def calc_score(self, num_cards):
+        print self,
         if self.tricks != self.bid:
+            print 'missed!',
             self.score += 0
-        if self.bid == 0:
+        elif self.bid == 0:
             self.score += 5 + num_cards
+            print 'made %s.' % 0,
         else:
             self.score += 10 + self.bid**2
+            print 'made %s.' % self.bid,
+        print '\tScore:',self.score
         self.tricks = 0
 
     def take_trick(self):
@@ -51,15 +57,36 @@ class Player(object):
         return 'Player' + str(self.id)
 
 class ManualPlayer(Player):
-    def play(self, suit):
-        pass
+    def play(self, lead_suit):
+        cards = []
+        legal = [c for c in self.hand if c.suit == lead_suit]
+        if not legal:
+            legal = self.hand
+        if len(legal) == 1:
+            cards = legal
+
+        while not cards:
+            self.display_hand()
+            (rank, suit) = raw_input('Choose a card to play: ').split()
+            cards = [c for c in legal if c.rank == Rank[rank] and c.suit == Suit[suit]]
+
+        card = cards[0]
+        print self,'plays',card
+        self.hand.remove(card)
+        return card
 
     def make_bid(self, forbidden=None):
-        pass
+        self.display_hand()
+        bid = forbidden
+        while bid not in [i for i in range(len(self.hand)+1) if i != forbidden]:
+            bid = int(raw_input('Bid anything but %s: ' % forbidden))
+
+        self.bid = bid
+        return self.bid
 
 class OhHell:
     def __init__(self, n):
-        self.players = [Player(i) for i in range(n)]
+        self.players = [Player(i) for i in range(n-1)]+[ManualPlayer(n-1)]
         self.deck = Deck()
         self.max_tricks = 52/len(self.players)
         self.num_tricks = 1
@@ -74,17 +101,20 @@ class OhHell:
         highests = [card for card in trick if card.suit == self.trump]
         if not highests:
             highests = [card for card in trick if card.suit == self.lead_suit]
-        return trick[max(highests, key=lambda x: (x.rank.value-1)%len(Rank))]
+        winner = trick[max(highests, key=lambda x: (x.rank.value-2)%len(Rank))]
+        print self.players[winner],'takes the trick!'
+        return winner
 
     # returns a card representing trump, or None for no trump
     def choose_trump(self):
+        trump_card = None
+        self.trump = None
         if self.round % 5 != 0 and self.num_tricks*len(self.players) != 52:
-            self.trump = self.deck.draw()[0].suit
-        else:
-            self.trump = None
+            trump_card = self.deck.draw()[0]
+            self.trump = trump_card.suit
 
         print '\n\n****************\nRound',self.round,'!'
-        print 'The trump is',self.trump
+        print '\t\t\t\tTRUMP:',trump_card
 
     def set_num_tricks(self):
         if self.round <= self.max_tricks:
@@ -107,17 +137,14 @@ class OhHell:
         self.players[self.dealer].make_bid(forbidden=self.num_tricks-total_bid)
 
         # play
-        leader = self.dealer
-        self.lead_suit = None
-        print 'num tricks',self.num_tricks
+        leader = (self.dealer+1) % len(self.players)
         for t in range(self.num_tricks):
             print '\nTrick',t
+            self.lead_suit = None
             trick = {}
             for i in range(len(self.players)):
                 player_index = (leader + i) % len(self.players)
                 player = self.players[player_index]
-                print player,"'s hand:"
-                player.display_hand()
 
                 card = player.play(self.lead_suit)
                 self.lead_suit = card.suit if not self.lead_suit else self.lead_suit
@@ -125,6 +152,7 @@ class OhHell:
             leader = self.find_winner(trick)
             self.players[leader].take_trick()
 
+        print '##### SCORES ######'
         for p in self.players:
             p.calc_score(self.num_tricks)
 
